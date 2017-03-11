f9ee210f8862e5728cf54b6c58b984c57fa18e30
SeanJackson1998 <skatts123@gmail.com>
2017-03-11 15:14:54 +0000

    Added reading from the .txt file and can get the next question

--- app/src/main/java/com/example/manav/tindermaths/question.java
+++ app/src/main/java/com/example/manav/tindermaths/question.java
@@ -1,5 +1,52 @@
 package com.example.manav.tindermaths;
 
-public class question  {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class question {
+
+    BufferedReader fileReader;
+
+    String question = "question doesn't exist";
+    String answer = "answer doesn't exist";
+    String questionLine = "";
+
+
+    public question(String filename) {
+        readFile(filename);
+    }
+
+    public String getSwipeQuestion() {
+
+        return question;
+    }
+
+    public void readFile(String filename) {
+        FileReader questionFile = null;
+
+        try {
+            questionFile = new FileReader(filename);
+            fileReader = new BufferedReader(questionFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Find Not Found");
+        }
+    }
+
+    public String[] getNextQuestion()
+    {
+        String[] questionLines = null;
+        try {
+            questionLine = fileReader.readLine();
+            questionLines = questionLine.split(",");
+            question = questionLines[0];
+            answer = questionLines[1];
+        } catch (IOException e) {
+            System.out.println("IOException in Read");
+        }
+        return questionLines;
+    }
 
 }
