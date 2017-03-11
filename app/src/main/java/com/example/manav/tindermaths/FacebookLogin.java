13fbeac94d23f6a29fa50abda64d45449f228c7b
conorm125 <cmalcolm88@hotmail.co.uk>
2017-03-11 20:35:42 +0000

    partly added fb stuff(TBC)

--- app/src/main/java/com/example/manav/tindermaths/FacebookLogin.java
+++ app/src/main/java/com/example/manav/tindermaths/FacebookLogin.java
@@ -1,7 +1,15 @@
 package com.example.manav.tindermaths;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class FacebookLogin extends AppCompatActivity {
 
@@ -10,4 +18,18 @@ public class FacebookLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
     }
+
+    //login button stuff
+/*
+    public class MainActivity extends FragmentActivity {
+        CallbackManager callbackManager;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            callbackManager = CallbackManager.Factory.create();
+            LoginButton loginButton = (LoginButton) view.findViewById(R.id.usersettings_fragment_login_button);
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });
+        }
+    }
+    */
 }
