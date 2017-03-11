580ce6dd17207d4536043b400c7b513cce62b6b4
conorm125 <cmalcolm88@hotmail.co.uk>
2017-03-11 14:49:04 +0000

    Start of quiz ui class

--- app/src/main/java/com/example/manav/tindermaths/QuizUI.java
+++ app/src/main/java/com/example/manav/tindermaths/QuizUI.java
@@ -5,9 +5,27 @@ import android.os.Bundle;
 
 public class QuizUI extends AppCompatActivity {
 
+    //Global variables
+    private String difficulty;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_ui);
+
+        //set difficulty text
+        Bundle b = getIntent().getExtras();
+        difficulty = b.getString("id");
+        //lblDifficulty.text = ("Difficulty: " + difficulty);
     }
+
+    /*
+    //constructor
+    public QuizUI(String )
+    {
+
+    }
+
+    */
 }
