056523e75e14ddf7ab0ef81dd3271295d939c379
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-03-12 05:05:18 +0000

    tries to do share button and fails

--- app/src/main/java/com/example/manav/tindermaths/EndGame.java
+++ app/src/main/java/com/example/manav/tindermaths/EndGame.java
@@ -3,19 +3,33 @@ package com.example.manav.tindermaths;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.share.DeviceShareDialog;
+import com.facebook.share.ShareApi;
+import com.facebook.share.Sharer;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EndGame extends AppCompatActivity {
 
@@ -27,6 +41,9 @@ public class EndGame extends AppCompatActivity {
     //counter
     private int counter = 0;
     String userID, userName;
+    private CallbackManager callbackManager;
+    private LoginManager loginManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +58,21 @@ public class EndGame extends AppCompatActivity {
         TVtime.setText(time);
         TextView TVscore = (TextView) findViewById(R.id.EndScore);
         TVscore.setText(score);
+        Button button = (Button) findViewById(R.id.share_btn);
+        final ShareDialog shareDialog = new ShareDialog(this);  // intialize facebook shareDialog.
+            button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if (ShareDialog.canShow(ShareLinkContent.class)) {
+                        ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                            .setContentTitle("How to integrate Linkedin from your app")
+                            .build();
+                   shareDialog.show(linkContent);  // Show facebook ShareDialog<br />
+                }
+            }
+        });
     }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)  {
@@ -64,13 +95,14 @@ public class EndGame extends AppCompatActivity {
     }
 
 
-    public void share(View view){
+    /*public void share(View view){
         //fb test
         Bitmap image = takeScreenShot(this);
         SharePhoto photo = new SharePhoto.Builder()
                 .setBitmap(image)
                 .build();
-    }
+    }*/
+
     private static Bitmap takeScreenShot(Activity activity)
     {
         View view = activity.getWindow().getDecorView();
